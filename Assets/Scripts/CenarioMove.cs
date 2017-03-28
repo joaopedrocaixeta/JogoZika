0af2aee9255b486e2c008f547ac30f66831fb8f1
Bruna Faria <fariabruna71@gmail.com>
2017-03-28 13:16:34 -0300

    Inserindo o cenario na cena

---
+++ Assets/Scripts/CenarioMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CenarioMove : MonoBehaviour {
+
+    public float velocidade = 1f;
+    public float limite;
+    public float retorno;
+
+	void Start () {
+	}
+	
+	void Update () {
+        Vector3 velocidadeVetorial = Vector3.left * velocidade;
+
+        transform.position = transform.position + velocidadeVetorial * Time.deltaTime;
+
+        if (transform.position.x <= limite){
+            transform.position = new Vector3(retorno, transform.position.y, transform.position.z);
+        }
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour {
+
+    public Estado estado { get; private set; }
+
+    public float espera;
+    public GameObject obstaculo;
+    public float tempoDestruicao;
+    
+    public static GameController instancia = null;
+
+	void Awake () {
+        if(instancia == null){
+            instancia = this;
+        }
+        else if (instancia != null){
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+	}
+
+    void Start(){
+        estado = Estado.AguardoComecar;
+    }
+
+    IEnumerator GerarObstaculos(){
+        while (true){
+            Vector3 pos = new Vector3(-27f, Random.Range(-5f, -5f), 0f);
+            GameObject obj = Instantiate(obstaculo, pos, Quaternion.Euler(0f,180f,0)) as GameObject;
+            Destroy(obj, tempoDestruicao);
+            yield return new WaitForSeconds(espera);
+        }
+        yield return null;
+    }
+
+    public void PlayerComecou(){
+        estado = Estado.Jogando;
+        StartCoroutine(GerarObstaculos());
+    }
+
+    public void PlayerMorreu(){
+        estado = Estado.GameOver;
+    }
+}
